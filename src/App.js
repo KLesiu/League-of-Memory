12cbf950e1fd8af2d3f7d4b4a3e0c2057a5f539e
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-03-25 17:57:45 +0100

    Add API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React,{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [champions,setChampions]=useState('')
+  useEffect(()=>{
+    console.log('siema')
+    fetch('http://ddragon.leagueoflegends.com/cdn/13.6.1/data/en_US/champion.json').then(res=> res.json()).then((response)=>{
+      setChampions(response.data)
+    })
+  },[])
   return (
     <div className="App">
-     
+     <button onClick={()=>{
+      console.log(champions)
+     }}></button>
     </div>
   );
 }
