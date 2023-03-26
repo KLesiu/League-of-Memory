cefea2b5496c431e32b24617fce8e228d895e8e7
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-03-26 16:29:08 +0200

    Add gameboard.js that will generate all champions and them photos. Add function generateArray that will create an array with name and image parameters

--- src/App.js
+++ src/App.js
@@ -2,22 +2,37 @@ import React,{useState,useEffect} from 'react';
 import './App.css';
 import Header from './components/Header';
 import Scoreboard from './components/Scoreboard';
+import Gameboard from './components/Gameboard';
 function App() {
-  const [champions,setChampions]=useState('')
+  const [champions,setChampions]=useState([])
   const [score,setScore]=useState(0)
   const [bestScore,setBestScore]=useState(0)
   const [list,setList]=useState([])
   useEffect(()=>{
-    console.log('siema')
+
     fetch('http://ddragon.leagueoflegends.com/cdn/13.6.1/data/en_US/champion.json').then(res=> res.json()).then((response)=>{
       setChampions(response.data)
+      
+     
     })
   },[])
+ const generateArray=()=>{
+  console.log(list)
+  const dataList=Object.keys(champions)
+ setList(dataList.map((element)=>{
+  return(
+    element={name:element,image:champions[element].image.full}
+    
+  ) 
+ }
+ ))
+ }
+
   return (
     <div className="App">
       <Header/>
       <Scoreboard score={{score:score,bestScore:bestScore}}/>
-      
+      <Gameboard generate={generateArray} list={list} />
     </div>
   );
 }
---
+++ src/components/Gameboard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Gameboard=(props)=>{
+
+return(
+    <button onClick={props.generate}></button>
+)
+}
+export default Gameboard
