2b57ba3563b3da8ba7a1257387100bf4919eb499
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-03-27 18:35:23 +0200

    Add function change which will generate new array to state list

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [score,setScore]=useState(0)
   const [bestScore,setBestScore]=useState(0)
   const [list,setList]=useState([])
+  
   useEffect(()=>{
 
     fetch('http://ddragon.leagueoflegends.com/cdn/13.6.1/data/en_US/champion.json').then(res=> res.json()).then((response)=>{
@@ -15,27 +16,36 @@ function App() {
       const champions=dataList.map((element)=>{
         return(
           
-          <Card image={response.data[element].image.full} name={element} />
+          <Card image={response.data[element].image.full} change={change} name={element} />
         )
       })
       setList(champions)
+   
       
 
      
     })
     
   },[])
-//  const generateArray=()=>{
-//   console.log(list)
-//   const dataList=Object.keys(champions)
-//  setList(dataList.map((element)=>{
-//   return(
-//     element={name:element,image:champions[element].image.full}
+  const change=()=>{
+ 
+    let currentIndex=[]
+    let arr=[]
+    for(let i=0;i<163;i++){
+      let numb=Math.floor(Math.random()*163)
+      currentIndex.push(numb)
+      if(currentIndex.includes(numb)){
+        while(currentIndex.includes(numb)){
+          numb=Math.floor(Math.random()*163)
+      
+        }
+      }
+      arr.push(numb)
+
+    }
+    console.log(arr)
     
-//   ) 
-//  }
-//  ))
-//  }
+  }
 
   return (
     <div className="App">
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,8 @@ import './styles/Card.css'
 const Card=(props)=>{
 
 return(
-    <div className="card">
+   
+    <div onClick={props.change} className="card">
         <img src={`http://ddragon.leagueoflegends.com/cdn/13.6.1/img/champion/${props.image}`} alt={props.name}></img>
         <h3>{props.name}</h3>
     </div>
